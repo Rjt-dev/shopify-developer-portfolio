140beb45b8ad761d80d84e0d24665d3d3c198770
Rajat Salaria <63835066+Rjt-dev@users.noreply.github.com>
2026-01-19 21:03:55 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll for nav links
-const links = document.querySelectorAll('nav a');
-links.forEach(link=>{
-  link.addEventListener('click',e=>{
-    e.preventDefault();
-    const target=document.querySelector(link.getAttribute('href'));
-    target.scrollIntoView({behavior:'smooth'});
-  });
-});
